4651762ce6772eab34f31dfc611c3d25f50c8393
Dani Ortunyo <danielortunyomurcia@gmail.com>
2025-03-18 20:30:03 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
         System.out.println("Hola, Dani!");
 
-        System.out.println("Hola, Adrian!");
+        System.out.println("Hola, YEeea!");
 
         System.out.println("Hola, a todos!");
     }
